3c1afba92c799c0b1ec56644f1407867098f8aa8
Francisco Sanchez <fransafu@gmail.com>
2021-07-03 23:45:28 -0400

    :constructor: Import ngx-markdown module and declare endpoint

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { WritePostComponent } from './pages/post/write-post/write-post.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/', pathMatch: 'full' },
   { path: '', component: HomeComponent },
+  { path: 'post/write', component: WritePostComponent },
   { path: '**', redirectTo: '/', pathMatch: 'full' }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,40 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import { MarkdownModule } from 'ngx-markdown';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { PostPreviewComponent } from './components/post-preview/post-preview.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+
+import 'prismjs';
+import 'prismjs/components/prism-typescript.min.js';
+import 'prismjs/plugins/line-numbers/prism-line-numbers.js';
+import 'prismjs/plugins/line-highlight/prism-line-highlight.js';
+
+import { WritePostComponent } from './pages/post/write-post/write-post.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    PostPreviewComponent
+    PostPreviewComponent,
+    WritePostComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatCardModule
+    FormsModule,
+    HttpClientModule,
+    MatCardModule,
+    MatButtonModule,
+    MarkdownModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
