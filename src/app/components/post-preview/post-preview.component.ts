e01876a1c678b8774e70d13beac7ec7a1e7c5703
Francisco Sanchez <fransafu@gmail.com>
2021-07-03 23:43:16 -0400

    feat: improve post preview design and added title input parameter

--- src/app/components/post-preview/post-preview.component.ts
+++ src/app/components/post-preview/post-preview.component.ts
@@ -7,6 +7,7 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class PostPreviewComponent implements OnInit {
   @Input() img: string = '';
+  @Input() title: string = '';
   @Input() content: string = '';
 
   constructor() { }
