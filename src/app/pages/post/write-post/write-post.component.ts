7a374ec8e8a15075019ee631b0f57b448960c9bd
Francisco Sanchez <fransafu@gmail.com>
2021-07-03 23:42:28 -0400

    :constructor: First version markdown editor

---
+++ src/app/pages/post/write-post/write-post.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-write-post',
+  templateUrl: './write-post.component.html',
+  styleUrls: ['./write-post.component.scss']
+})
+export class WritePostComponent implements OnInit {
+
+  markdown = `## Markdown __rulez__!
+---
+
+### Syntax highlight
+\`\`\`typescript
+const language = 'typescript';
+\`\`\`
+
+### Lists
+1. Ordered list
+2. Another bullet point
+   - Unordered list
+   - Another unordered bullet
+
+### Blockquote
+> Blockquote to the max`;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
